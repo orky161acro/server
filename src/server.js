1743e332d0d23e1d40a671c9dc7c0844fad4ca1c
orky161 <orky161@gmail.com>
2021-01-30 13:28:11 +0200

    add compression middleware

--- src/server.js
+++ src/server.js
@@ -2,12 +2,14 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const apiRouter = require('./api/index');
+const compression = require('compression')
 
 const app = express()
 const port = 8080
 
 app.use(cors())
 app.use(bodyParser.json())
+app.use(compression())
 app.use('/api', apiRouter);
 
 
