dcc452f20b4eb3f0b1810ff7cbe2f5ddaf6d8cb9
orky161 <orky161@gmail.com>
2021-01-31 14:45:36 +0200

    expose mysqlConnection

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const router = express.Router();
 const v1Router = require('./v1/index');
+const {checkConnection} = require("./v1/dal/dalInitaliztor");
 
 router.use('/v1', v1Router);
+
+router.get('/ping', function (req, res) {
+    res.status(200).json({msg: "ping"})
+})
+
+router.get('/health', async (req, res) => {
+    await checkConnection()
+    res.status(200).json({msg: "health"})
+})
 
 module.exports = router
--- src/config/index.js
+++ src/config/index.js
@@ -4,7 +4,7 @@ const mysqlConnection = {
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
-}
+};
 
 module.exports = {
     mysqlConnection
